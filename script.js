0fbbf42dcff9253b6985725914e8241eba417d90
amanda <amandalphail@gmail.com>
2021-03-11 22:44:52 -0500

    cleaned up final js

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-// https://data.cityofnewyork.us/resource/8knb-zsb6.json
-
 
 const locationDD = document.querySelector('.location')
 const featuresDD =document.querySelector('.features')
-// const option = document.querySelector('option')
 const searchResults = document.querySelector('#sr-main')
 const resultsPage = document.querySelector('#search-results')
 let submit = document.querySelector(".form")
-// let homepage = document.querySelector("#homepage")
 let main = document.querySelector('main')
 let srHeader = document.querySelector('#sr-header')
 let button = document.querySelector(".randomPark")
@@ -20,18 +16,7 @@ async function getSkateParks(event){
     try{
         const response = await axios.get('https://data.cityofnewyork.us/resource/8knb-zsb6.json')
 
-        console.log(response)
-        // console.log(response.data[0].borough)
-        
-        // if (locationDD.value === "null" || featuresDD.value === "null"){
-        //     removeResults()
-        //     console.log("one selector is null")
-
-        // } else 
-        console.dir()
-
-
-        // if(form submitted){}
+    
         if(event.type === "submit"){
         if (locationDD.value !== "null" && featuresDD.value === "null"){
             setLocation(response.data)
@@ -44,21 +29,10 @@ async function getSkateParks(event){
         }
     }
 
-
-
-        
-        // // if (button === 'click'){
-        // getRandomPark(response.data)
-        // // }
-                       
-         
-
-
          
         return response.data
     } catch(err){
-        console.log(err)
-        console.log(err.message)
+
     }
 }
 
@@ -69,8 +43,7 @@ async function getRandomParks(event){
     const parksArray = await getSkateParks(event)
     console.log(parksArray)
     const randomPark = parksArray[Math.floor(Math.random() * parksArray.length)]
-         
-    //  console.log(randomPark.name)
+
     displaySetUp()
     displayResults(randomPark)
 }
@@ -84,20 +57,15 @@ function setLocation (data){
     displaySetUp()
     for (i = 0; i < data.length; i++){
         let borough = data[i].borough
-        // let name = data[i].name
         let value = data[i]
         let locOption = locationDD.value
         
 
         if (featuresDD.value === "null" && borough.includes(locOption)){
-        // if (borough.includes(locOption)){
-            // console.log(name)
-            // displayLocation(name)
+
             displayResults(value)
         } 
-        // else {
-        //     // getBoth(data)
-        // }
+  
      }
 }
 
@@ -108,24 +76,14 @@ function setFeature(data){
         let feature1 = data[i].features_1
         let feature2 = data[i].features_2
         let feature3 = data[i].features_3
-        // let name = data[i].name
         let value = data[i]
         let featOption = featuresDD.value
-        // console.log(borough)
         
         if(locationDD.value === "null" && (feature1.includes(featOption) || feature2.includes(featOption) || feature3.includes(featOption))){
-            // console.log("location null and feature selected")
-            // displayFeatures(name)
+
             displayResults(value)
-            console.log(data[i].borough)
         }  
-        // else {
-        //     //  getBoth(data)
-        //  }
-        // if (feature1.includes(featOption) || feature2.includes(featOption) || feature3.includes(featOption)){
-        //     getFeatures(name)
-        //     console.log(data[i].borough)
-        // }
+
 
      }
 }
@@ -140,44 +98,19 @@ function getBoth(data){
         let feature2 = data[i].features_2
         let feature3 = data[i].features_3
         let featOption = featuresDD.value
-        // let name = data[i].name
         let value = data[i]
 
         if(borough.includes(locOption) && (feature1.includes(featOption) || feature2.includes(featOption) || feature3.includes(featOption))){
             displayResults(value)
-            // displayBoth(name)
-            // console.log(name)
+
         }
     }
 }
-
-// function getLocation(event){
-//     console.log(event)
-    
-//     displayLocation(event)
-// }
-
-// function getFeatures(event){
-//     console.log(event)
-    
-//     displayFeatures(event)
-// }
-
-// locationDD.addEventListener('change', getSkateParks)
-// featuresDD.addEventListener('change', getSkateParks)
-
-
 
 
 
 submit.addEventListener('submit', getSkateParks)
 
-// form.onsubmit = function submit(){
-//     return false;
-    //keeps page from refreshing when hit submit
-// }
-// https://stackoverflow.com/questions/19454310/stop-form-refreshing-page-on-submit 
-// https://www.w3schools.com/jsref/event_onsubmit.asp
 
 
 function createResultHeader(){
@@ -210,9 +143,6 @@ function createResultHeader(){
 
 }
 
-// function createSearchResults(){
-//     let srMain = document.createElement()
-// }
 
 
 function displaySetUp(){
@@ -229,13 +159,10 @@ function displaySetUp(){
 
 function displayResults(value){
 
-
-
     let leftDIV = document.createElement('div')
     leftDIV.classList.add('leftDIV')
     let rightDIV = document.createElement('div')
     rightDIV.classList.add('rightDIV')
-
     let result = document.createElement('div')
     result.classList.add('result')
 
@@ -246,8 +173,6 @@ function displayResults(value){
     let resultLocation = document.createElement('p')
     resultLocation.classList.add('result-location')
     resultLocation.innerText = `Location: ${value.borough}`
-
-
 
 
     let resultFeat1 = document.createElement('p')
@@ -267,59 +192,19 @@ function displayResults(value){
     leftDIV.appendChild(resultLocation)
     result.appendChild(leftDIV)
 
-
-    
     rightDIV.appendChild(resultFeat1)
     rightDIV.appendChild(resultFeat2)
     rightDIV.appendChild(resultFeat3)
     result.appendChild(rightDIV)
     
     searchResults.appendChild(result)
-    console.log(value)
 
-    
-
-    // let experiment = document.createElement('div')
-
-    // searchResults.insertBefore(experiment, searchResults.children[2])
-
-
-    // resultsPage.insertBefore(experiment,resultsPage.children[3])
 }
-
-// function displayLocation(location){
-//     let result = document.createElement('p')
-//     result.innerText = location
-//     result.classList.add('result')
-//     searchResults.appendChild(result)
-//     console.log(location)
-
-// }
-
-// function displayFeatures(feature){
-//     let result = document.createElement('p')
-//     result.innerText = feature
-//     result.classList.add('result')
-//     searchResults.appendChild(result)
-//     console.log(feature)
-// }
-
-// function displayBoth(both){
-//     let result = document.createElement('p')
-//     result.innerText = both
-//     result.classList.add('result')
-//     searchResults.appendChild(result)
-//     console.log(both)
-// }
-
-
-
 
 
 
 function removeResults (){
 
-    // while we have first child (aka parent isnt empty) - remove first child
     while(searchResults.firstChild){
         searchResults.removeChild(searchResults.firstChild)
     }
@@ -342,9 +227,5 @@ function removeSearchResultsPage(){
         body.removeChild(body.lastChild)
     
 }
-
-
-
-
 
 
